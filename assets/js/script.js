622948f443b495b8773d54f84051752fef2c4195
Luke Poirrier <lukepoirrier5@gmail.com>
2021-07-28 14:13:54 -0400

    Initial commit - API call initiated

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+var APIKey = "fe02176d7651716d37adb2b5b5ee7209";
+var city = "Atlanta";
+var dayAmt = 6;
+var lat = 0;
+var lon = 0;
+var mainUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&units=imperial&appid=" + APIKey;
+var firstUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ APIKey;
+
+// ~! Fetches coordinates for the searched city and stores them in the lat & lon variables
+function getCoordinates(){
+      fetch(firstUrl)
+      .then(function(response){
+            return response.json();
+      })
+      .then(function(data){
+            lat = data.coord.lat;
+            lon = data.coord.lon;
+            console.log(lat + " --- " + lon)
+      })
+}
+
+function getWeather(){
+      fetch(mainUrl)
+      .then(function(response){
+            return response.json();
+      })
+      .then(function(data){
+            for(day in data.daily){
+                  console.log("Date: " + moment("1627128000").format("MM/DD/YY")) //data.daily[day]
+                 console.log("UV Index: " + data.daily[day].uvi)
+                  console.log("Temperature: " + data.daily[day].temp.day)
+            }    
+      })
+} 
+      getCoordinates();
+      getWeather();
