77e519ecc6f379e78f57386ee0b01b8e2a1e4378
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-05 18:28:22 -0400

    Add degrees symbol to current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,7 +100,7 @@ function getWeather(lat, lon) {
       var currentContent = `<div class="card-body bg-secondary" id="currentWeatherCard">
       <h2 class="font-weight-bold card-title"><span id="cityName"></span> <span id="currentDate">${month}/${day}/${year}</span>
       <span id="weatherIcon"><img src="https://openweathermap.org/img/wn/${iconURL}.png" id="weatherI"></span></h2>
-      <p class="card-text currentWeather">Temp: <span id="currentTemp">${temp}</span></p>
+      <p class="card-text currentWeather">Temp: <span id="currentTemp">${temp}°</span></p>
       <p class="card-text currentWeather">Wind Speed: <span id="currentWind">${wind}</span></p>
       <p class="card-text currentWeather">Humidity: <span id="currentHumidity">${humidity}</span></p>
       <p class="card-text currentWeather">UVI: <span id="currentUVI">${uvi}</span></p>
